e50ef84b453be765f821394186c09d6f7960ade2
Michael Davydov <nixesvfx@gmail.com>
2022-03-24 16:23:44 +0300

    Database cleanup

--- database.py
+++ database.py
@@ -35,6 +35,9 @@ class BroModel(Model):
     date_deleted = DateTimeField(null=True)
     deleted = BooleanField(default=False)
 
+    class Meta:
+        database = db
+
     def mark_deleted(self):
         self.deleted = True
         self.date_deleted = datetime.now()
@@ -43,8 +46,8 @@ class BroModel(Model):
     def save(self, *args, **kwargs):
         if self.date_created is None:
             self.date_created = datetime.now()
-        if self.date_updated is None:
-            self.date_updated = datetime.now()
+            
+        self.date_updated = datetime.now()
 
         super(BroModel, self).save(*args, **kwargs)
 
@@ -52,46 +55,6 @@ class BroModel(Model):
 class Entry(BroModel):
     filename = TextField()
 
-    class Meta:
-        database = db
-
-    def create(self, **query):
-        log.debug("Creating new Entry: " + " ".join([self.title]))
-        ret = super(Entry, self).create(**query)
-        return ret
-
-    def save(self, *args, **kwargs):
-        log.debug("Updating Entry info: " + str("") + " - " + str(self.title))
-        self.date_updated = datetime.now()
-        ret = super(Entry, self).save(*args, **kwargs)
-
-        return ret
-
-
-# region Migration
-config = read_config()
-if config['database_migrate']:
-    log.debug("=====================")
-    log.debug("Migration stuff...")
-    try:
-        from playhouse.migrate import *
-
-        migrator = SqliteMigrator(db)
-
-        open_count = IntegerField(default=0)
-
-        migrate(
-            migrator.add_column('Entry', 'open_count', open_count)
-        )
-        log.debug("Migration success")
-        log.debug("=====================")
-
-        config['database_migrate'] = False
-        write_config(config)
-    except:
-        log.error("Could not migrate", exc_info=True)
-        log.debug("=====================")
-# endregion
 
 log.info(" ".join(["Using DB", str(db), "At path:", str(db_path)]))
 
